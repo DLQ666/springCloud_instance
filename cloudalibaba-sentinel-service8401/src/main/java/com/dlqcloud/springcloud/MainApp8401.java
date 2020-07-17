eee560f5982b954bbd609c879159e09720078bc1
D·Teach <1114626450@qq.com>
2020-07-17 22:30:26 +0800

    添加了一部分Sentinel熔断与限流实例工程

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/dlqcloud/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.dlqcloud.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: cloud01
+ * @description: 主启动类
+ * @author: Hasee
+ * @create: 2020-07-17 16:43
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
